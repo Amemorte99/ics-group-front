cf2087fe437e223b52ec0deec82284c42e5502e6
KOI-SONN MANES MANA <koisonnmanes@gmail.com>
2025-12-22 14:41:22 +0100

    main

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2bcee80a074babc08f21"; }
+/******/ 		__webpack_require__.h = function() { return "174fa03d7b26e032d16d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
-    },
-    trailingSlash: true,
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  trailingSlash: true,
+
+  // ⚡️ Configuration pour rendre `next export` compatible avec <Image>
+  images: {
+    loader: 'akamai',  // ou 'imgix', 'cloudinary', selon ton choix
+    path: '',          // nécessaire pour `next export`
+  },
+};
